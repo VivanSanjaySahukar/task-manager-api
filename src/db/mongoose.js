6f878bfbac916b2cfd836b148cb8d0809dbc96fe
vivan <vivansanjaysahukar@gmail.com>
2021-05-25 20:07:34 +0530

    Init commit.

---
+++ src/db/mongoose.js
@@ -0,0 +1,12 @@
+//this js file is used to connect to the database
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+})
+
+
+
